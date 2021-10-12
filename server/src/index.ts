074b6cc4cb613a956762a8310ea641bb6160205c
fernandoapeguero <fernandoapeguero@gmail.com>
2021-10-12 00:25:41 -0400

    Typescript support added   Added typescript support to the project and tsconfig file.   typescript added for type enforcement.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
-const express = require("express");
+import express from "express";
 
 const port = 9000;
 const app = express();
+
+const one: number = 10;
+const two: number = 2;
+const three: boolean = true;
 
 app.get("/", (req, res) => {
   res.send("hello world");
